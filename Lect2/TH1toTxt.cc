30c9c3826d936ea0d5aeaa9bf4d50181f10ac3c1
mastrovalentz <alevalentz@gmail.com>
2019-12-13 12:06:40 +0100

    Lect2 v3

--- Lect2/TH1toTxt.cc
+++ Lect2/TH1toTxt.cc
@@ -1,5 +1,5 @@
 void TH1toTxt (TH1F *h, string outfile) {
 	ofstream out (outfile);
-	n=h->GetNbinsX();
-	for (int i=0; i<n; i++) out<<h->GetBinCenter(i)<<"\t"<<h->GetBinCenter(i)<<endl;
+	int n=h->GetNbinsX();
+	for (int i=0; i<n; i++) out<<h->GetBinCenter(i)<<"\t"<<h->GetBinContent(i)<<endl;
 }
