0b9073c6bf24445a6f3dfa2d10d3144551ee5629
mastrovalentz <alevalentz@gmail.com>
2019-12-13 12:33:10 +0100

    Ex2  v2

---
+++ Lect2/TxttoTGraph.cc
@@ -0,0 +1,11 @@
+void TxttoTGraph (string name) {
+	TGraph * gr= new TGraph((name+".txt").c_str());
+	TCanvas *c= new TCanvas("c", "c", 600, 600);
+	c->cd();
+	gr->Draw();
+	c->Draw();
+	c->Print((name+".png").c_str());
+}
+
+//to execute in batch
+//root -l -b 'TxttoTGraph.cc ("hAllout"); exit (0)'
